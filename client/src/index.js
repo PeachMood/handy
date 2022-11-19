9e66163db1a805f8799896554a19c83f85196551
PeachMood <vas140302@gmail.com>
2022-11-20 02:46:32 +0700

    Initialized react project with webpack

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const HelloWorld = () => {
+    return (
+        <h1>
+            Hello World
+        </h1>
+    );
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
