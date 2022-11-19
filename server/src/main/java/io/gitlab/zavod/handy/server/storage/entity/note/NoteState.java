417e0a996ba0ccfb1cd07e47f8900663f2b56ff1
dtolmachev1 <d.tolmachev1@g.nsu.ru>
2022-11-20 01:14:37 +0700

    HANDY-27. Interfaces for storage level

---
+++ server/src/main/java/io/gitlab/zavod/handy/server/storage/entity/note/NoteState.java
@@ -0,0 +1,10 @@
+package io.gitlab.zavod.handy.server.storage.entity.note;
+
+/**
+ * <p>Represents current note state.</p>
+ */
+public enum NoteState {
+    TRASHED,    // In trash, but not deleted
+    DELETED,    // Permanently deleted
+    ACTIVE    // Currently exist
+}
