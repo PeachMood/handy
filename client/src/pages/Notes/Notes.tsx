1d1321e0ed609faf13434b62d02a38c8441ea41f
black_raven <a.voronova1@g.nsu.ru>
2023-06-02 04:32:41 +0700

    HANDY-66. Not working editor

--- client/src/pages/Notes/Notes.tsx
+++ client/src/pages/Notes/Notes.tsx
@@ -7,16 +7,51 @@ import { Sidebar } from './Sidebar/Sidebar';
 import styles from './Notes.module.scss';
 import { ContextMenu } from './ContextMenu/ContextMenu';
 import { ConfirmPopup } from './ConfirmPopup/ConfirmPopup';
+import { useNotesApi } from 'hooks/api/useNotesApi';
 
 export const Notes: FC = () => {
-  const { currentNote } = useNotesContext();
+  const { currentNote, setCurrentNote, updateNote } = useNotesContext();
+  const { getNote, editNote } = useNotesApi();
+  const [content] = useState<string>(currentNote?.content);
+
+  const handleChange = async (content: string) => {
+    try {
+      const note = { ...currentNote, content };
+      const response = await editNote(note);
+      // updateNote(response.data);
+      // setCurrentNote(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      try {
+        const response = await getNote(currentNote.id);
+        updateNote(response.data);
+        setCurrentNote(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    const sendRequestInInterval = async () => {
+      let interval = setInterval(sendRequest, 10000);
+      return () => {
+        clearInterval(interval);
+      };
+    }
+
+    sendRequestInInterval();
+  }, []);
 
   return (
     <div className={styles.page}>
       <Sidebar />
       <div className={styles.wrapper}>
         <header className={styles.header} />
-        <Editor className={styles.editor} content={currentNote?.content} />
+        <Editor className={styles.editor} content={content} onChange={handleChange} />
       </div>
       <ContextMenu />
       <ConfirmPopup />
